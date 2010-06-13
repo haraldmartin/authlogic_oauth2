9b3453833a638f1508a76d40bac2b9249bc75283
Andrew Hite <andrew@andrew-hite.com>
2010-06-13 13:01:06 -0500

    Removed everything so I can check-in the gem version of authlogic_oauth2

--- lib/authlogic_oauth2.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + "/authlogic_oauth2/version"
-require File.dirname(__FILE__) + "/authlogic_oauth2/oauth2_process"
-require File.dirname(__FILE__) + "/authlogic_oauth2/acts_as_authentic"
-require File.dirname(__FILE__) + "/authlogic_oauth2/session"
-require File.dirname(__FILE__) + "/authlogic_oauth2/helper"
-
-ActiveRecord::Base.send(:include, AuthlogicOauth2::ActsAsAuthentic)
-Authlogic::Session::Base.send(:include, AuthlogicOauth2::Session)
-ActionController::Base.helper AuthlogicOauth2::Helper
