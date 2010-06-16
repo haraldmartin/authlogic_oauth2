213086f4f4839bf801a478004282e57a9819be85
Andrew Hite <andrew@andrew-hite.com>
2010-06-16 14:19:07 -0500

    Simplified the inclusion of the callback filter in the middleware stack

--- lib/authlogic_oauth2.rb
+++ lib/authlogic_oauth2.rb
@@ -11,15 +11,7 @@ ActionController::Base.helper AuthlogicOauth2::Helper
 
 # Throw callback rack app into the middleware stack
 if defined?(ActionController::Metal)
-  # Rails >= 3.0
-  require 'oauth2_callback_filter'
-  if Rails.application.instance_variable_get('@app')
-    Rails.application.instance_variable_set('@app', Oauth2CallbackFilter.new(Rails.application.app))
-  else
-    Rails.configuration.middleware.use(Oauth2CallbackFilter)
-  end
-elsif defined?(ActionController::Dispatcher) && defined?(ActionController::Dispatcher.middleware)
-  # Rails >= 2.3
-  require 'oauth2_callback_filter'
-  ActionController::Dispatcher.middleware.use(Oauth2CallbackFilter)
+  Rails.configuration.middleware.use(Oauth2CallbackFilter) # Rails >= 3.0
+else
+  ActionController::Dispatcher.middleware.use(Oauth2CallbackFilter) # Rails < 3.0
 end
